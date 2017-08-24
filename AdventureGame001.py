dd58392ba656ec687ec1eda044ac590cd8d8f3a4
Patrick Orel <Dsrpaygroon@users.noreply.github.com>
2017-08-23 22:41:52 -0500

    AdventureGame002.py
    edit on naming room values to putting it in a list, and added create room function

--- AdventureGame001.py
+++ AdventureGame001.py
@@ -1,58 +1,86 @@
 import random
 
-roomX = 5
-roomY = 5
-userX = 1
-userY = 1
-doorX = 3
-doorY = 5
+# [[roomX, roomY], [UserX, UserY], [[Door1X, Door1Y], [Door2X, Door2Y], etc.]
+roomValues = [[5,5],[1,1],[3,5]]
 answerDoor = "That is a door"
 answerWall = "That is a wall"
 answerEmpty = "You walked, good job"
 
 def move(direction):
-    global roomX
-    global roomY
-    global userX
-    global userY
-    global doorX
-    global doorY
+    global roomValues
     if direction.lower() == "up":
-        if userY + 1 > roomY:
+        if roomValues[1][1] + 1 > roomValues[0][1]:
             print answerWall
         else:
-            userY += 1
-            if userY == doorY and userX == doorX:
+            roomValues[1][1] += 1
+            if roomValues[1][1] == roomValues[2][1] and roomValues[1][0] == roomValues[2][0]:
                 print answerDoor
             else:
                 print answerEmpty
     elif direction.lower() == "down":
-        if userY - 1 < 1:
+        if roomValues[1][1] - 1 < 1:
             print answerWall
         else:
-            userY -= 1
-            if userY == doorY and userX == doorX:
+            roomValues[1][1] -= 1
+            if roomValues[1][1] == roomValues[2][1] and roomValues[1][0] == roomValues[2][0]:
                 print answerDoor
             else:
                 print answerEmpty
     elif direction.lower() == "left":
-        if userX - 1 < 1:
+        if roomValues[1][0] - 1 < 1:
             print answerWall
         else:
-            userX -= 1
-            if userY == doorY and userX == doorX:
+            roomValues[1][0] -= 1
+            if roomValues[1][1] == roomValues[2][1] and roomValues[1][0] == roomValues[2][0]:
                 print answerDoor
             else:
                 print answerEmpty
     elif direction.lower() == "right":
-        if userX + 1 > roomX:
+        if roomValues[1][0] + 1 > roomValues[2][0]:
             print answerWall
         else:
-            userX += 1
-            if userY == doorY and userX == doorX:
+            roomValues[1][0] += 1
+            if roomValues[1][1] == roomValues[2][1] and roomValues[1][0] == roomValues[2][0]:
                 print answerDoor
             else:
                 print answerEmpty
+                
+def useDoor():
+    global roomValues
+    print "You walk through the doorway into a new room."
+    roomValues = createRoom(roomValues)
     
-def createRoom():
-    print 0
+def createRoom(roomValues):
+    roomValues[0][0] = random.randint(3,10)
+    roomValues[0][1] = random.randint(3,10)
+    door = random.randint(1,4)
+    
+    if roomValues[1][1] == roomValues[2][1] and roomValues[1][1] == roomValues[0][1]:
+        #If door was on the top
+        roomValues[1][1] = 1
+    elif roomValues[1][0] == roomValues[2][0] and roomValues[1][0] == roomValues[0][1]:
+        #If door was on the right
+        roomValues[1][0] = 1
+    elif roomValues[1][1] == 1 and roomValues[2][1] == 1:
+        #If door was on the bottem
+        roomValues[1][1] = roomValues[0][1]
+    else:
+        #If door was on the left
+        roomValues[1][0] = roomValues[0][0]
+            
+    if door == 1:
+        #If door is on the top
+        roomValues[2][0] = random.randint(1, roomValues[0][0])
+        roomValues[2][1] = roomValues[0][1]
+    elif door == 2:
+        #If door is on the right
+        roomValues[2][0] = roomValues[0][0]
+        roomValues[2][1] = random.randint(1, roomValues[0][1])
+    elif door == 3:
+        #If door is on the bottem
+        roomValues[2][0] = random.randint(1, roomValues[0][0])
+        roomValues[2][1] = 1
+    else:
+        #If door is on the left
+        roomValues[2][0] = 1
+        roomValues[2][1] = random.randint(1, roomValues[0][1])
